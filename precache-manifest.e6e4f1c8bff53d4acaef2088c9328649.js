7e695c9709441169e220c3d231a03e55dc3a04ab
rachelsmantra <rachelannewilliams13@gmail.com>
2020-02-20 19:43:58 -0600

    Updates

---
+++ precache-manifest.e6e4f1c8bff53d4acaef2088c9328649.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319aea46b483d9528ea1a6b893c8821c",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "03e8a6de4c185ac83a69",
+    "url": "/todolist/static/css/main.56f61a5c.chunk.css"
+  },
+  {
+    "revision": "6a7fa1c4622f4bddfe0e",
+    "url": "/todolist/static/js/2.4048e49a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todolist/static/js/2.4048e49a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03e8a6de4c185ac83a69",
+    "url": "/todolist/static/js/main.3c564a36.chunk.js"
+  },
+  {
+    "revision": "cebccde0f7f76a903cc9",
+    "url": "/todolist/static/js/runtime-main.6fa7f924.js"
+  }
+]);
