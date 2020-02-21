6d1c3a9c6ebcd82048c1be0e439a4fba4a6ae566
rachelsmantra <rachelannewilliams13@gmail.com>
2020-02-20 18:45:59 -0600

    further refactoring into components

--- src/App.js
+++ src/App.js
@@ -1,57 +1,12 @@
 import React, { useState } from "react";
-
+import Todo from "./Components/Todo/todo";
+import TodoForm from "./Components/Todoform/todo-form";
 import "./App.css";
-
-function Todo({ todo, index, completeTodo, removeTodo }) {
-  return (
-    <div
-      className="todo"
-      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
-    >
-      {todo.text}
-
-      <div>
-        <button onClick={() => completeTodo(index)}>Complete</button>
-        <button onClick={() => removeTodo(index)}>x</button>
-      </div>
-    </div>
-  );
-}
-
-function TodoForm({ addTodo }) {
-  const [value, setValue] = useState("");
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (!value) return;
-    addTodo(value);
-    setValue("");
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="input"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-      />
-    </form>
-  );
-}
 
 function App() {
   const [todos, setTodos] = useState([
     {
-      text: "Learn about React",
-      isCompleted: false
-    },
-    {
-      text: "Meet friend for lunch",
-      isCompleted: false
-    },
-    {
-      text: "Build really cool todo app",
+      text: "",
       isCompleted: false
     }
   ]);
